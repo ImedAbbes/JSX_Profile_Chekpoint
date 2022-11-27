f82a3e397162fea48121c19d9bd00685ea750494
ImedAbbes <imed.abbes@gmail.com>
2022-11-27 16:12:54 +0100

    a5iran

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Address() {
+  return (
+    <div className='address'>
+      <p>Rue Sayeb el La3ba N 52</p>
+    </div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image from './avatar.png'
+function ProfilePhoto() {
+  return (
+    <div>
+      <img src={image} alt="avatar"/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
